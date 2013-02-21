b6a89f4dafe72dfef573b2a8ded17881677b614c
Cameron Bytheway <cameron@nujii.com>
2013-02-21 11:50:18 -0700

    Clean up a few things with superagent

--- index.js
+++ index.js
@@ -2,23 +2,26 @@
 /**
  * Module dependencies
  */
-var superagent = require("superagent")
-  , http = require('http');
+var superagent = require("superagent");
 
 // Don't let superagent serialize anything
 superagent.serialize = {};
 
-module.exports = function(endpoint) {
+module.exports = function(endpoint, options) {
+  if (!options) options = {};
   return function simpleHttpProxy(req, res, next) {
-    // Remove the host header
     var hostInfo = req.headers.host.split(":")
       , host = hostInfo[0]
       , port = hostInfo[1]
       , resPath = req.originalUrl.replace(req.url, "");
+
+    // Remove the host header
     delete req.headers.host;
 
     // Optionally delete cookie
-    delete req.headers.cookie;
+    if(!options.cookie) {
+      delete req.headers.cookie;
+    }
 
     // We'll need to add a / if it's not on there
     if(resPath.indexOf("/") === -1) resPath = "/"+resPath;
@@ -26,14 +29,19 @@ module.exports = function(endpoint) {
     // Send it through superagent
     var request = superagent(req.method, endpoint+req.url)
       .buffer(false)
-      .set(req.headers)
-      .set({
-        "x-forwarded-host": host,
-        "x-forwarded-proto": req.connection.encrypted ? "https" : "http",
-        "x-forwarded-path": resPath
-      });
+      .set(req.headers);
 
-    if (port) request.set("x-forwarded-port", port);
+    // Add the x-forwarded-* headers
+    if(options.xforward) {
+      request
+        .set({
+          "x-forwarded-proto": req.connection.encrypted ? "https" : "http",
+          "x-forwarded-host": host,
+          "x-forwarded-path": resPath
+        });
+
+      if (port) request.set("x-forwarded-port", port);
+    }
 
     // Pipe upstream and downstream
     req.pipe(request);
